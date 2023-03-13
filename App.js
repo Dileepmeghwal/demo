9d016c70df2f30ca5886fb6ca78b40de28f33bc5
dileepmeghwal <dileepmeghwalrock275@gmail.com>
2023-03-14 00:00:04 +0530

    calendar

--- App.js
+++ App.js
@@ -1,39 +1,77 @@
 import React, { useState } from "react";
-import { SafeAreaView, Button, Text, View } from "react-native";
-import Calendar from "./app/Calendar";
-import { Agenda } from "react-native-calendars";
+import {
+  SafeAreaView,
+  Button,
+  Text,
+  View,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import Calendar from "react-native-calendars/src/calendar";
 
-export default function App() {
+const App = () => {
+  const [showModal, setshowModal] = useState(false);
+  const [events, setEvents] = useState([
+    {
+      start: '2023-03-16 00:00:00',
+      end: '2023-03-17 02:00:00',
+      title: 'New Year Party',
+      summary: 'xyz Location',
+    },
+    
+  ]);
+
+  const vacation = { key: "vacation", color: "red", selectedDotColor: "blue" };
+  const massage = { key: "massage", color: "blue", selectedDotColor: "blue" };
+  const workout = { key: "workout", color: "green" };
   return (
     <View style={styles.container}>
-      <Calendar
-        markingType={"period"}
-        markedDates={{
-          "2012-05-15": { marked: true, dotColor: "#50cebb" },
-          "2012-05-16": { marked: true, dotColor: "#50cebb" },
-          "2012-05-21": {
-            startingDay: true,
-            color: "#50cebb",
-            textColor: "white",
-          },
-          "2012-05-22": { color: "#70d7c7", textColor: "white" },
-          "2012-05-23": {
-            color: "#70d7c7",
-            textColor: "white",
-            marked: true,
-            dotColor: "white",
-          },
-          "2012-05-24": { color: "#70d7c7", textColor: "white" },
-          "2012-05-25": {
-            endingDay: true,
-            color: "#50cebb",
-            textColor: "white",
-          },
+      <TouchableOpacity
+        onPress={() => setshowModal(true)}
+        style={{
+          backgroundColor: "black",
+          borderRadius: 10,
+          margin: 40,
+          padding: 10,
+          width: 200,
+          alignItems: "center",
         }}
-      />
+      >
+        <Text style={{ color: "#fff", fontSize: 15 }}>Show Calendar</Text>
+      </TouchableOpacity>
+      <Modal visible={showModal} animationType="fade">
+        <Calendar
+          style={{ padding: 20, margin: 20, elevation: 4, borderRadius: 10 }}
+          onDayPress={(date) => {
+            alert(date);
+            setshowModal(false);
+            
+          }}
+          initialDate={"2023-03-15"}
+          hideExtraDays={true}
+          // markedDates={{
+          //   "2023-03-15": {marked: true, dotColor: 'blue', selected:true, selectedColor: 'purple',selectedTextColor: '#fff', }
+          // }}
+          // markingType={'multi-dot'}
+          // markedDates={{
+          //   "2023-03-17":{dots:[{color:'red'}, {color:'green'}]}
+          // }}
+
+          markingType={"multi-dot"}
+          markedDates={{
+            "2023-03-15": {
+              dots: [vacation, massage, workout],
+              selected: true,
+              selectedColor: "red",
+            },
+            "2023-03-26": { dots: [massage, workout], disabled: true },
+          }}
+        />
+        <Text>{events.title}</Text>
+      </Modal>
     </View>
   );
-}
+};
 
 const styles = (StyleSheet = {
   container: {
@@ -48,3 +86,4 @@ const styles = (StyleSheet = {
     textAlign: "center",
   },
 });
+export default App;
